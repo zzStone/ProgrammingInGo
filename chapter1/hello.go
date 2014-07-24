dbbb61e9b3720e590219b32fb0c4b634fe488045
Jizhou Li <jizhou.li@crowdsurge.com>
2014-07-24 11:50:11 -0400

    change hello.go to allow arguments

--- chapter1/hello.go
+++ chapter1/hello.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("hello, GO world")
+	who := "world"
+	if len(os.Args) > 1 {
+		who = strings.Join(os.Args[1:], " ")
+	}
+	fmt.Println("hello", who)
 }
