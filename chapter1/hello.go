3bf7b55190a8560640c137308f2d705ac6b036d8
Jizhou Li <jizhou.li@crowdsurge.com>
2014-07-25 11:01:41 -0400

    add stack stucture

--- chapter1/hello.go
+++ chapter1/hello.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+    "github.com/zzStone/ProgrammingInGo/chapter1/stacker"
 )
 
 func main() {
@@ -12,4 +13,17 @@ func main() {
 		who = strings.Join(os.Args[1:], " ")
 	}
 	fmt.Println("hello", who)
+
+    var haystack stacker.Stack
+    haystack.Push("hay")
+    haystack.Push(-15)
+    haystack.Push([]string{"ala", "bit", "da"})
+    haystack.Push(81.52)
+    for {
+        item, err := haystack.Pop()
+        if err != nil {
+            break
+        }
+        fmt.Println(item)
+    }    
 }
